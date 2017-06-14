7a50bd874dc7c04c6091dde781e9434a3e0fc600
Lucia Walinchus <Lucia@professionalnewsservices.com>
2017-06-14 13:06:02 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -38,7 +38,7 @@ def scrape_table(root):
         print table_cells
         if table_cells: 
         #if there is a cell, record the contents in our dataset, the first cell [0] in 'recipient' and so on
-            record['Charge'] = table_cells2.text_content()
+            record['Charge'] = table_cells.text_content()
             #record['Date Filed and Judge'] = table_cells[1].text_content()
             #this line adds 1 to the ID no. we set at 0 earlier
             #idno=idno+1

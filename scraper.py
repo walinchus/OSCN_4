76992755c6ccceb1a436214851c3e26bbc24ace7
Lucia Walinchus <Lucia@professionalnewsservices.com>
2017-06-14 14:51:04 -0500

    Create scraper.py

--- scraper.py
+++ scraper.py
@@ -32,11 +32,11 @@ def scrape_table(root):
         #create a record to hold the data
         #record = {}
         #for each row, loop through this
-        for row in rows:
+        for row in rows(0,len(rows)):
             #create a list of all cells <td> in that row
             table_cells = row.cssselect("td")
             if table_cells:
-                for EachCharge in table_cells(0,len(table_cells)):
+                for EachCharge in table_cells:
                 #if there is a cell, record the contents in our dataset, the first cell [0] in 'recipient' and so on
                     record['Charge'] = EachCharge.text_content() #table_cells[x].text_content()
                     #record['Date Filed and Judge'] = table_cells[1].text_content()

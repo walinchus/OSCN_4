994470d5e02f1ce0b78ec9438028de243599fc4b
Lucia Walinchus <Lucia@professionalnewsservices.com>
2017-06-15 14:28:00 -0500

    Create scraper.py

--- scraper.py
+++ scraper.py
@@ -28,7 +28,7 @@ def scrape_table(root):
             #idno=idno+1
             #record['ID'] = idno 
             print record, '------------'
-        counts = root.cssselect("div.sized div.CountsContainer")
+        counts = root.cssselect("div.CountsContainer div")
         countstotal = len(counts)
         print "total number of counts:", countstotal
         #countsrange = range(0, countstotal+1)

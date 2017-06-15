2c99f8a9f2d7e29f67d01cce91ae6bfcac69c616
Lucia Walinchus <Lucia@professionalnewsservices.com>
2017-06-15 13:41:00 -0500

    Create scraper.py

--- scraper.py
+++ scraper.py
@@ -49,9 +49,9 @@ def scrape_table(root):
                     #create a list of all cells <td> in that row
                     table_cells = row.cssselect("td")
                     print table_cells
-                    #record['Charges'] = table_cells.text_content()
-                    #record['Count Description:'] = table_cells[1].text_content()
-                    #record['Outcome:'] = table_cells[2].textcontent()
+                    record['Charges'] = table_cells[0].text_content()
+                    record['Count Description:'] = table_cells[1].text_content()
+                    record['Outcome:'] = table_cells[3].textcontent()
                     print record, '------------'
                         # Save the record to the datastore - 'ID' is our unique key - '''
     print 'ALL DATA:', record
